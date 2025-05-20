f9a0db23f6ae1183ebde805fc87ea077588d5a25
mashayeakh <islammasayekh@gmail.com>
2025-05-20 23:39:10 +0600

    done with navbar and hero img

---
+++ src/components/Home/HeroSection.jsx
@@ -0,0 +1,74 @@
+import React from 'react'
+
+const HeroSection = () => {
+    return (
+        <>
+
+            <div
+                className="hero min-h-screen"
+                style={{
+                    backgroundImage:
+                        "url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp)",
+                }}
+            >
+                <div className="hero-overlay"></div>
+                <div className="hero-content text-neutral-content text-center">
+                    <div className="max-w-md">
+                        <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+                        <p className="mb-5">
+                            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+                            quasi. In deleniti eaque aut repudiandae et a id nisi.
+                        </p>
+                        <button className="btn btn-primary">Shop Now</button>
+                    </div>
+                </div>
+            </div>
+            <div className='py-5 flex justify-around'>
+                <div className="card bg-base-100 image-full w-96 shadow-sm">
+                    <figure>
+                        <img
+                            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                            alt="Shoes" />
+                    </figure>
+                    <div className="card-body">
+                        <h2 className="card-title">Smart Phone</h2>
+                        <p></p>
+                        <div className="card-actions justify-end">
+                            <button className="btn btn-primary">Buy Now</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="card bg-base-100 image-full w-96 shadow-sm">
+                    <figure>
+                        <img
+                            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                            alt="Shoes" />
+                    </figure>
+                    <div className="card-body">
+                        <h2 className="card-title">Smart Phone</h2>
+                        <p></p>
+                        <div className="card-actions justify-end">
+                            <button className="btn btn-primary">Buy Now</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="card bg-base-100 image-full w-96 shadow-sm">
+                    <figure>
+                        <img
+                            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                            alt="Shoes" />
+                    </figure>
+                    <div className="card-body">
+                        <h2 className="card-title">Smart Phone</h2>
+                        <p></p>
+                        <div className="card-actions justify-end">
+                            <button className="btn btn-primary">Buy Now</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default HeroSection
